11461b0de47db1d0fdab8c96370f21c817345901
John <mwxfranco@gmail.com>
2023-04-08 18:13:04 +0100

    took some inspiration from lz77, and used pointers instead of storing offsets. the pointers work differently to lz77 tho, so i ain't copying nothing.

---
+++ compression_core.py
@@ -0,0 +1,129 @@
+def generate_tokens_and_offsets(raw_input: str):
+    # generate all tokens and offsets
+    tokens = []
+    for i in range(3, 4):
+        window_size = i
+        for j in range(len(raw_input) - window_size):
+            tokens.append((raw_input[j:j + window_size], j))
+    print(f'Generated tokens: {len(tokens)} tkns')
+
+    # group all the tokens into a dictionary
+    grouped_tokens = {}
+    for token in tokens:
+        if token[0] not in grouped_tokens:
+            grouped_tokens[token[0]] = [token[1]]
+        else:
+            grouped_tokens[token[0]].append(token[1])
+    print(f'Grouped tokens')
+
+    def filter_tokens(tokens_dict: dict):
+        """
+        Token length | Number of offsets required for compression to be positive
+        -------------|----------------------------------------------------------
+        3            | 5
+        4            | 3
+        5+           | 2
+        As the token length increases, the number of pointers needed in the final output decreases, until only 2 are
+        required to have a net positive reduction in file size.
+        :param tokens_dict:
+        :return:
+        """
+        filtered_tokens = {}
+        for k, v in tokens_dict.items():
+            token_length = len(k)
+            if token_length == 3 and len(v) >= 5:
+                filtered_tokens[k] = v
+            if token_length == 4 and len(v) >= 3:
+                filtered_tokens[k] = v
+            if token_length >= 5 and len(v) >= 2:
+                filtered_tokens[k] = v
+        return filtered_tokens
+
+    # filter tokens
+    filtered_tokens = filter_tokens(grouped_tokens)
+    print(f'Filtered tokens: {sum(len(offsets) for offsets in filtered_tokens.values())} tkns')
+
+    # flatten token dictionary into ordered list, ordered by offset
+    flattened_tokens = []
+    for token, offsets in filtered_tokens.items():
+        flattened_token_list = [(token, off) for off in offsets]
+        flattened_tokens += flattened_token_list
+    sorted_tokens = sorted(flattened_tokens, key=lambda item: item[1])
+    print(f'Flattened tokens')
+
+    def within_bounds(offset, offset_length, offset2, offset2_length):
+        if offset2 <= offset <= offset2 + offset2_length:
+            return True
+        if offset2 <= offset + offset_length <= offset2 + offset2_length:
+            return True
+        return False
+
+    # remove overlapping tokens
+    print('Removing redundant tokens: ', end='')
+    number_of_tokens = len(sorted_tokens)
+    interval = number_of_tokens // 41
+    progress_bar = ['0', '.', '.', '.', '1', '.', '.', '.', '2', '.', '.', '.', '3', '.', '.', '.', '4', '.', '.', '.',
+                    '5', '.', '.', '.', '6', '.', '.', '.', '7', '.', '.', '.', '8', '.', '.', '.', '9', '.', '.', '.',
+                    '10']
+    progress_bar_index = 0
+
+    unique_tokens = []
+    token_counter = 0
+    for token in sorted_tokens:
+        if not unique_tokens:
+            unique_tokens.append(token)
+            continue
+        last_token = unique_tokens[-1]
+        unique_token_offset = last_token[1]
+        token_offset = token[1]
+        if not within_bounds(token_offset, len(token[0]), unique_token_offset, len(last_token[0])):
+            unique_tokens.append(token)
+
+        # increment progress bar if needed
+        token_counter += 1
+        if token_counter == interval:
+            print(progress_bar[progress_bar_index], end='')
+            progress_bar_index += 1
+            token_counter = 0
+    print(f'\nRemoved redundant tokens: {len(unique_tokens)} tkns')
+
+    # The tokens are in an ordered list, so regroup them
+    grouped_tokens = {}
+    for token in unique_tokens:
+        if token[0] not in grouped_tokens:
+            grouped_tokens[token[0]] = [token[1]]
+        else:
+            grouped_tokens[token[0]].append(token[1])
+    print(f'Regrouped tokens')
+
+    # The redundancy check may have resulted in tokens having some of their offsets removed, so refilter the tokens
+    filtered_tokens = filter_tokens(grouped_tokens)
+    print(f'Refiltered tokens: {sum(len(offsets) for offsets in filtered_tokens.values())} tkns')
+
+    return filtered_tokens
+
+
+def create_pointer(token_key_index: int):
+    """
+    The pointer is 2 bytes, representing a number from 0-65536
+    In order to identify a byte as the first half of a pointer, make the first bit always be 1:
+    10000000 00000000
+    ^ here
+    ASCII characters are 7 bits, which allow for the encoding of all printable characters. So the first bit of an ASCII
+    byte is always a 0. Example:
+    'a' = 01100001
+          ^ here
+    So to differentiate between an ASCII character and a pointer, flip that first bit to a 1.
+    This results in pointers being 2 bytes.
+
+    The rest of the 15 bits are used for the token index, from 0-32768.
+    So a file can have up to 32768 different tokens, which is plenty.
+    :param token_key_index:
+    :return:
+    """
+    pointer_bytes = token_key_index.to_bytes(2, 'little')
+    bit_strings = [format(byte, '08b') for byte in pointer_bytes]  # convert bytes to bit strings
+    first_bit_string = bit_strings[0]
+    bit_strings[0] = '1' + first_bit_string[1:]  # flip the first bit to a 1
+    marked_pointer_bytes = bytes([int(bit_strings[0], 2), int(bit_strings[1], 2)])  # convert bit strings back to bytes
+    return marked_pointer_bytes
--- compressor.py
+++ compressor.py
@@ -1,112 +1,46 @@
 import sys
 
+from compression_core import generate_tokens_and_offsets, create_pointer
+
 with open(sys.argv[1]) as f:
     input_text = f.read()
 
-# generate all tokens and offsets
-tokens = []
-for i in range(3, 5):
-    window_size = i
-    for j in range(len(input_text) - window_size):
-        tokens.append((input_text[j:j + window_size], j))
-print(f'Generated tokens: {len(tokens)} tkns')
-
-# group all the tokens into a dictionary
-grouped_tokens = {}
-for token in tokens:
-    if token[0] not in grouped_tokens:
-        grouped_tokens[token[0]] = [token[1]]
-    else:
-        grouped_tokens[token[0]].append(token[1])
-print(f'Grouped tokens: {len(grouped_tokens)} tkns')
-
-# sort by descending, filtering out tokens with only two occurences
-print('Sorting tokens')
-sorted_tokens = dict(sorted(grouped_tokens.items(), key=lambda item: len(item[1]), reverse=True))
-filtered_tokens = {k: v for k, v in sorted_tokens.items() if len(v) > 2}
-print(f'Filtered tokens: {len(filtered_tokens)} tkns')
-
-# adjust offsets for each token, removing tokens which are too far apart
-print('Reducing offset sizes')
-adjusted_tokens = {}
-for token, offsets in filtered_tokens.items():
-    new_offsets = []
-    for i in range(len(offsets)):
-        if i:
-            diff = offsets[i] - offsets[i - 1]
-            if diff < 256:
-                new_offsets.append(diff)
-        else:
-            new_offsets.append(offsets[i])
-    adjusted_tokens[token] = new_offsets
-
-# refilter out tokens with only two occurrences
-filtered_tokens = {k: v for k, v in adjusted_tokens.items() if len(v) > 2}
-print(f'Refiltered tokens: {len(filtered_tokens)} tkns')
-
-
-def overlaps(token1, off1: int, token2, offsets2):
-    total_offset = 0
-    for off2 in offsets2:
-        total_offset += off2
-        if off2 + total_offset <= off1 <= off2 + total_offset + len(token2):
-            return True
-        if off2 + total_offset <= off1 + len(token1) <= off2 + total_offset + len(token2):
-            return True
-    return False
-
-
-# remove overlapping tokens
-unique_tokens = {}
-for token, offsets in filtered_tokens.items():
-    unique_offsets = []
-    total_offset = 0
-    for off in offsets:
-        total_offset += off
-        is_offset_unique = True
-        for u_token, u_offsets in unique_tokens.items():
-            if overlaps(token, off + total_offset, u_token, u_offsets):
-                is_offset_unique = False
-                break
-        if not is_offset_unique:
-            break
-        if is_offset_unique:
-            unique_offsets.append(off)
-    unique_tokens[token] = unique_offsets
-print(f'Removed redundant tokens: {len(unique_tokens)} tkns')
-
-filtered_tokens = {k: v for k, v in unique_tokens.items() if len(v) > 2}
-print(f'Refiltered tokens: {len(filtered_tokens)} tkns')
+generated_tokens = generate_tokens_and_offsets(input_text)
 
 with open('tokens.txt', 'w') as f:
-    for key, value in filtered_tokens.items():
+    for key, value in generated_tokens.items():
         f.write(f'{key}: {value}\n')
 
-print('Compressing input text')
-bytes_text = input_text.encode()
-for token, offsets in filtered_tokens.items():
-    total_offset = 0
-    for offset in offsets:
-        total_offset += offset
-        bytes_text = bytes_text[:total_offset] + b'\x00' * len(token) + bytes_text[total_offset + len(token):]
+# sort the tokens in ascending order according to their offsets
+tokens = []
+for token, offsets in generated_tokens.items():
+    for off in offsets:
+        tokens.append((token, off))
+sorted_tokens = sorted(tokens, key=lambda item: item[1])
 
-compressed_text = bytes_text.translate(None, b'\x00')
+token_keys = list(generated_tokens.keys())
+compressed_text = b''
+prev_index = 0
+for i in range(len(sorted_tokens)):
+    token = sorted_tokens[i]
+    prev_text = input_text[prev_index: token[1]].encode()
+    token_key_index = token_keys.index(token[0])
+    pointer = create_pointer(token_key_index)
+    compressed_text += prev_text + pointer
+    prev_index = token[1] + len(token[0])
 
-print('Generating metadata')
-metadata = b''
-for token, offsets in filtered_tokens.items():
-    token_data = token.encode()
-    for offset in offsets:
-        token_data += offset.to_bytes((offset.bit_length() // 8) + 1, 'big')
-    token_data += b'\x00'
-    metadata += token_data
+# separate each token with a null byte.  0x02 is used to identify the end of the metadata
+metadata = b'\x00'.join(token.encode() for token in token_keys) + b'\x02'
 
 print('Writing to file')
 with open('compressed.txt', 'wb') as f:
     f.write(metadata + compressed_text)
 
+print()
+print(f'Metadata size: {len(metadata)} bytes')
+print(f'Compressed text size: {len(compressed_text)} bytes')
 size_before = len(input_text)
-size_after = len(metadata) + len(compressed_text)
+size_after = len(metadata + compressed_text)
 print(f'Size before: {size_before} bytes')
 print(f'Size after: {size_after} bytes')
 print(f'Saving: {size_before - size_after} bytes')
